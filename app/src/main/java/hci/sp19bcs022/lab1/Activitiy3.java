236f80a684de24a9b6639825416a9e584e6cfb0d
Abdullah <abdullah.javed9438>
2022-04-10 11:49:09 +0500

    Initial commit

---
+++ app/src/main/java/hci/sp19bcs022/lab1/Activitiy3.java
@@ -0,0 +1,14 @@
+package hci.sp19bcs022.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activitiy3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activitiy3);
+    }
+}
